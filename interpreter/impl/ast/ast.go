d08d5833e371cc82398d5fb07983d84c7ec4257a
Haffi Mazhar <haffimazhar96@gmail.com>
2023-06-25 12:50:36 +0100

    2.5

--- interpreter/impl/ast/ast.go
+++ interpreter/impl/ast/ast.go
@@ -28,6 +28,7 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+// Let Statement
 type LetStatement struct {
 	Token token.Token // the token.LET token
 	Name  *Identifier
@@ -44,3 +45,12 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+// Return Statement
+type ReturnStatement struct {
+	Token       token.Token // the 'return' token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode()       {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
